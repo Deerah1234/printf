1e4ace2595d073782fd8187841695e349eeacb30
Emmanuel James <93952415+Deerah1234@users.noreply.github.com>
2022-11-22 08:47:25 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -78,15 +78,26 @@ int print_string(va_list types, char buffer[],
 }
 /************************* PRINT PERCENT SIGN *************************/
 /**
- * print_percent -  print percent character
- * @args: char
- * Return: percent character
+ * print_percent - Prints a percent sign
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
  */
-int print_percent(va_list args __attribute__((unused)))
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	return (_putchar('%'));
+	UNUSED(types);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return (write(1, "%%", 1));
 }
-
 /************************* PRINT INT *************************/
 /**
  * print_int - Print int
